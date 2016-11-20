1386462a22abf985613587adc078a5369fefaa50
hupeng <hupeng@imudges.com>
2016-11-20 20:49:51 +0800

    完成了较大的更新,更加简洁

--- src/com/example/odlutil/Main.java
+++ src/com/example/odlutil/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         //System.out.print(Base64.getEncoder().encodeToString(("admin" + ":" + "123456").getBytes()));
-        OdlUtil odlUtil = new OdlUtil("127.0.0.1",8080);
-        odlUtil.getTopology();
+        OdlUtil odlUtil = new OdlUtil("127.0.0.1",8080,"admin","admin","default");
+        odlUtil.getHosts();
     }
 }
--- src/com/example/odlutil/OdlUtil.java
+++ src/com/example/odlutil/OdlUtil.java
@@ -3,30 +3,68 @@ package com.example.odlutil;
 import java.util.Base64;
 
 /**
- * Created by dxc on 16-11-15.
+ * Created by hupeng on 16-11-15.
  */
 public class OdlUtil {
     private String url = "";
+    private String username;
+    private String password;
+    private String containerName;
 
-    public OdlUtil(String host,int port){
+    /**
+     * 构造函数
+     * @param host OpenDayLight的北向接口的主机名称
+     * @param port OpenDayLight的北向接口的端口
+     * @param username OpenDayLight的管理员的用户名
+     * @param password OpenDayLight的管理员的密码
+     * @param containerName OpenDayLight的容器名称
+     * */
+    public OdlUtil(String host,int port,String username,String password, String containerName){
         this.url = "http://" + host + ":" + port;
+        this.username = username;
+        this.password = password;
+        this.containerName = containerName;
     }
 
+    /**
+     * 构造函数
+     * @param host OpenDayLight的北向接口的主机名称
+     * @param port OpenDayLight的北向接口的端口
+     * @param username OpenDayLight的管理员的用户名
+     * @param password OpenDayLight的管理员的密码
+     * */
+    public OdlUtil(String host,int port,String username,String password){
+        this.url = "http://" + host + ":" + port;
+        this.username = username;
+        this.password = password;
+        this.containerName = "default";
+    }
+
+    /**
+     * 获取拓扑信息
+     * */
     public String getTopology(){
-        return getTopology("default");
-    }
-
-    public String getTopology(String containerName, String username, String password) {
         HttpRequest.setBasicAuth(getBasicAuthStr(username,password));
         String str = HttpRequest.sendGet(url + "/controller/nb/v2/topology/" + containerName,"");
-        System.out.print(str);
-        return null;
+        //System.out.print(str);
+        return str;
     }
 
-    public String getTopology(String containerName){
-        getTopology(containerName, "admin","admin");
-        return null;
+
+
+    /**
+     * 获取主机信息
+     * */
+    public String getHosts(){
+        HttpRequest.setBasicAuth(getBasicAuthStr(this.username,this.password));
+        ///controller/nb/v2/topology/' + str(container_name)
+        String str = HttpRequest.sendGet(url + "/controller/nb/v2/topology/" + containerName,"");
+
+        System.out.println(str);
+
+        return str;
     }
+
 
     private String getBasicAuthStr(String name,String password){
         return "Basic " + Base64.getEncoder().encodeToString((name + ":" + password).getBytes());
